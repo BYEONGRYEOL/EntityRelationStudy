2d4e7da1064262bc06525f371a5c2348a09e6878
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-10 17:39:51 +0900

    onetoone Entity 생성

---
+++ src/main/java/com/sparta/jpaadvance/entity/FoodOneWayOwner.java
@@ -0,0 +1,21 @@
+package com.sparta.jpaadvance.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "food")
+public class FoodOneWayOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
+    @OneToOne
+    @JoinColumn(name = "userOneWayDependent_id")
+    private UserOneWayDependent userOneWayDependent;
+}
