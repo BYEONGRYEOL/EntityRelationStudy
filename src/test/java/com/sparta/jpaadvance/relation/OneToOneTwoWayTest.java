f063b77f53f5cb7e290ae1a78a75d2809b3a9c2e
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-10 18:46:27 +0900

    OneToOneTwoWayTest 주인 기준 조회, 주인을 조회 테스트 작성

--- src/test/java/com/sparta/jpaadvance/relation/OneToOneTwoWayTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/OneToOneTwoWayTest.java
@@ -71,6 +71,29 @@ public class OneToOneTwoWayTest {
     }
 
 
+    @Test
+    @DisplayName("1대1 조회 : Owner 기준 Dependent 정보 조회")
+    void findDependentByOwnerTest() {
+        FoodOTOTwoWayOwner food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
+        // 음식 정보 조회
+        System.out.println("food.getName() = " + food.getName());
+
+        // 음식을 주문한 고객 정보 조회
+        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+    }
+
+    @Test
+    @DisplayName("1대1 조회 : dependent 기준 owner 정보 조회")
+    void findOwnerByDependentTest() {
+        UserOTOTwoWayDependent user = userRepository.findById(1L).orElseThrow(NullPointerException::new);
+        // 고객 정보 조회
+        System.out.println("user.getName() = " + user.getName());
+
+        // 해당 고객이 주문한 음식 정보 조회
+        FoodOTOTwoWayOwner food = user.getFood();
+        System.out.println("food.getName() = " + food.getName());
+        System.out.println("food.getPrice() = " + food.getPrice());
+    }
     FoodOTOTwoWayOwner getFood() {
         return new FoodOTOTwoWayOwner(FOOD_NAME, 100);
     }
